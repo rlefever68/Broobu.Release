d84fc49d7931c2b6dfb0d8a05f6081b90742010f
Rafael Lefever <rafael.lefever@gmail.com>
2014-10-09 21:24:55 +0200

    Initial Release

---
+++ Wulka.SDK/Wulka.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wulka.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03d9b54f-deb1-491b-95c5-cff9af490147")]
+
+
+[assembly: AssemblyCompany("Broobu Systems")]
+[assembly: AssemblyProduct("Wulka SDK Test Code")]
+[assembly: AssemblyCopyright("Copyright © Broobu Systems 2013-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("14.10.09.142")]
+
